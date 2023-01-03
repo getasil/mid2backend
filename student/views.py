46f253a03b02b89e2816b4244de85feca43ff206
melaku <melakudagnew1921@gmail.com>
2023-01-03 09:58:48 +0300

    first commit

---
+++ student/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from . import models
+from . import serializers
+
+class teacherViewset(viewsets.ModelViewSet):
+    queryset=models.teacher.objects.all()
+    serializer_class=serializers.teacherSerializer
